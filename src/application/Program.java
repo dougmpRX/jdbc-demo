14ab587b9e1807621ee8a9b742c3b47220aee43b
Douglas Massote <douglasmpestana@gmail.com>
2023-02-19 16:26:54 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dept = new Department(1, "IT");
+		System.out.println(dept);
+		
+	}
+
+}
