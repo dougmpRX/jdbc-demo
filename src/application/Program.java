cbe689efcee81ce380872fcafcff646361773dae
Douglas Massote <douglasmpestana@gmail.com>
2023-02-19 16:34:33 -0300

    Creates Seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,9 @@ public class Program {
 		Department dept = new Department(1, "IT");
 		System.out.println(dept);
 		
+		Seller seller = new Seller(20, "John", "john@gmail.com", new Date(), 3500.0, dept);
+		System.out.println(seller);
+
 	}
 
 }
